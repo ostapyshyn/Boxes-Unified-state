3349bbfc6d156fdeb452eff0ed8f180f2067cdd9
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-03-17 17:29:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import boxes from "./boxes"
+import Box from "./Box"
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [squares, setSquares] = React.useState(boxes)
+    
+    function toggle(id) {
+        // setSquares(prevSquares => {
+        //     return prevSquares.map((square) => {
+        //         return square.id === id ? {...square, on: !square.on} : square
+        //     })
+        // })
+        setSquares(prevSquares => {
+            let newSquares = [...prevSquares];
+            newSquares[id - 1].on = !newSquares[id - 1].on;
+            return newSquares;
+        })
+    }
+    
+    const squareElements = squares.map(square => (
+        <Box 
+            key={square.id} 
+            on={square.on} 
+            toggle={(evt) => {
+              toggle(square.id);
+              console.log(evt);
+            }} //  toggle={(evt) => toggle(square.id)}
+        />
+    ))
+    
+    return (
+        <main className="field">
+            {squareElements}
+        </main>
+    )
 }
-
-export default App;
---
+++ src/Box.js
@@ -0,0 +1,18 @@
+import React from "react"
+import './index.css';
+
+
+export default function Box(props) {
+    const styles = {
+        backgroundColor: props.on ? "#222222" : "transparent"
+    }
+    
+    return (
+        <div 
+            style={styles} 
+            className="box"
+            onClick={props.toggle}
+        >
+        </div>
+    )
+}
